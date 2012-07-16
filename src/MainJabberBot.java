ee22c0a239044661597524ce74b8e186a7091fcc
Commaster <commastergm@gmail.com>
2012-07-16 14:35:05 +0400

    Lost update
    Signed-off-by: Commaster <commastergm@gmail.com>

--- src/MainJabberBot.java
+++ src/MainJabberBot.java
@@ -34,6 +34,7 @@ class JabberBot implements Runnable
     String Password = account.Password;
     String Domain = account.Domain;
     String mucName = account.mucName;
+    String thingy;
     
     String help = "rssbot@qip.ru - XMPP(jabber) бот, рассылающий новостные RSS ленты, оформленные в BB-коды.\n" +
 			"--------------------------------------------------------\n" +
